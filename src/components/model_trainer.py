c98a879c91de6aac260e3847cfc6f642c457f49d
Shubham <shubhamgupta43567@gmail.com>
2026-05-01 01:59:43 +0530

    model training done and tested.

---
+++ src/components/model_trainer.py
@@ -0,0 +1,55 @@
+from src.exception.exception import CustomException
+from src.logger.logger import logger
+import os
+import sys
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+import pickle
+from dataclasses import dataclass
+
+@dataclass
+class ModelTrainerConfig:
+  transformed_data_dir = os.path.join("artifacts", "transformed_data")
+  book_pivot = os.path.join(transformed_data_dir, "book_pivot.pkl")
+  book_names = os.path.join(transformed_data_dir, "book_names.pkl")
+  trained_model_dir = os.path.join("artifacts", "trained_model")
+  trained_model_name = os.path.join(trained_model_dir, "model.pkl")
+  
+  
+class ModelTrainer:
+  def __init__(self):
+    self.model_trainer_config = ModelTrainerConfig()
+    
+  def model_trainer(self):
+    try:
+      logger.info("Model trainer started")
+      
+      book_pivot = pickle.load(open(self.model_trainer_config.book_pivot, 'rb'))
+      book_names = pickle.load(open(self.model_trainer_config.book_names, 'rb'))
+      logger.info("Pickle files loaded")
+      
+      book_sparse = csr_matrix(book_pivot)
+      logger.info(f"Sparse matrix created. type(book_sparse): {type(book_sparse)}")
+      
+      model = NearestNeighbors(algorithm= 'brute')
+      model.fit(book_sparse)
+      logger.info("Model trained")
+      
+      os.makedirs(self.model_trainer_config.trained_model_dir, exist_ok=True)
+      pickle.dump(model, open(self.model_trainer_config.trained_model_name, 'wb'))
+      logger.info("Model saved")
+      
+      logger.info("Model trainer completed")
+      
+      
+    except Exception as e:
+      logger.error("Failed while initiating model trainer")
+      raise CustomException(e, sys)
+      
+  
+  def initiate_model_trainer(self):
+    try:
+      self.model_trainer()
+    except Exception as e:
+      logger.error("Failed while initiating model trainer")
+      raise CustomException(e, sys)
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,6 +1,7 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 
 
@@ -9,9 +10,11 @@ class TrainingPipeline:
     self.data_ingestion = DataIngestion()
     self.data_validation = DataValidation()
     self.data_transformation = DataTransformation()
+    self.model_trainer = ModelTrainer()
     
   def start_training_pipeline(self):
     """Start the training pipeline and return none"""
     self.data_ingestion.initiate_data_ingestion()
     self.data_validation.initiate_data_validation()
     self.data_transformation.initiate_data_transformation()
+    self.model_trainer.initiate_model_trainer()
